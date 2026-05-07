c886f545d124fa0c7daf702c56ab84af59cad34c
ZiyaIzaanAnam <85607187+ZiyaIzaanAnam@users.noreply.github.com>
2026-05-07 11:33:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -538,3 +538,10 @@ document.body.addEventListener("keydown", function (event) {
         }
     }
 });
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./sw.js")
+            .then(() => console.log("Service Worker registered"))
+            .catch(err => console.log("SW registration failed:", err));
+    });
+}
